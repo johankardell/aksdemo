343dc318de8745d7c9c6fd0e5843ca27aa58b3ee
Johan Kardell <johan.kardell@gmail.com>
2023-10-03 09:43:47 +0200

    Use deployment stacks

--- containerInsights/deploy.sh
+++ containerInsights/deploy.sh
@@ -1,3 +1,5 @@
 sshKey=`cat ~/.ssh/id_rsa.pub`
 
-az deployment sub create --location swedencentral --template-file main.bicep -n aksdemo --parameters sshkey="$sshKey"
+# az deployment sub create --location swedencentral --template-file main.bicep -n aksdemo --parameters sshkey="$sshKey"
+
+az stack sub create --deny-settings-mode none --delete-all --location swedencentral --template-file main.bicep -n aksdemo --parameters sshkey="$sshKey" --yes
