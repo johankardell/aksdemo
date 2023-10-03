3bbeb1a1d4b18110e582b1719ca954749fcfe29d
Johan Kardell <johan.kardell@gmail.com>
2023-10-03 09:44:31 +0200

    Remove nowait

--- containerInsights/terminate.sh
+++ containerInsights/terminate.sh
@@ -1,3 +1,3 @@
 # az group delete --name rg-aks-containerinsights-demo --yes --no-wait
 
-az stack sub delete --delete-all -n aksdemo --yes --no-wait
+az stack sub delete --delete-all -n aksdemo --yes
