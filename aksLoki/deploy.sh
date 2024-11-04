b6f5e5401bfdb067b234fbedf4bf6d8780596b3b
Johan Kardell <johan.kardell@gmail.com>
2024-11-04 15:28:15 +0100

    script updates

--- aksLoki/deploy.sh
+++ aksLoki/deploy.sh
@@ -5,6 +5,7 @@ RESOURCE_GROUP_NAME="rg-aks-loki"
 SYS_NODEPOOL_NAME="system"
 APPS_POOL_NAME="apps"
 LOKI_POOL_NAME="loki"
+WINDOWS_POOL_NAME="win"
 SA_NAME="jklokidemo"
 SA_RG="rg-aks-loki"
 SA_SUB=$(az account show --name four --query id -o tsv)
@@ -20,43 +21,60 @@ az aks create \
     --ssh-access disabled \
     --enable-image-cleaner \
     --nodepool-name $SYS_NODEPOOL_NAME \
+    --os-sku AzureLinux \
     --node-vm-size Standard_B4ms
 
 az aks nodepool update \
-        --cluster-name $CLUSTER_NAME \
-        --resource-group $RESOURCE_GROUP_NAME \
-        --name $SYS_NODEPOOL_NAME \
-        --node-taints CriticalAddonsOnly=true:NoSchedule
+    --cluster-name $CLUSTER_NAME \
+    --resource-group $RESOURCE_GROUP_NAME \
+    --name $SYS_NODEPOOL_NAME \
+    --node-taints CriticalAddonsOnly=true:NoSchedule
 
 az aks nodepool add \
     --cluster-name $CLUSTER_NAME \
     --resource-group $RESOURCE_GROUP_NAME \
     --name $APPS_POOL_NAME \
     --node-count 1 \
-    --node-vm-size Standard_D4s_v5 \
-    --node-osdisk-size 30 \
+    --node-vm-size standard_d4ads_v5 \
+    --node-osdisk-size 120 \
     --ssh-access disabled \
     --max-pods 250 \
+    --os-sku AzureLinux \
     --no-wait
-
-az aks nodepool update \
-    --cluster-name $CLUSTER_NAME \
-    --resource-group $RESOURCE_GROUP_NAME \
-    --name $APPS_POOL_NAME \
-    --labels nginx="true"
 
 az aks nodepool add \
     --cluster-name $CLUSTER_NAME \
     --resource-group $RESOURCE_GROUP_NAME \
     --name $LOKI_POOL_NAME \
     --node-count 3 \
-    --node-vm-size Standard_D4s_v5 \
-    --node-osdisk-size 30 \
+    --node-vm-size standard_d4ads_v5 \
+    --node-osdisk-size 120 \
     --ssh-access disabled \
     --max-pods 250 \
     --labels loki="true" \
     --zones 1 2 3 \
+    --os-sku AzureLinux \
     --no-wait
+
+az aks nodepool add \
+    --cluster-name $CLUSTER_NAME \
+    --resource-group $RESOURCE_GROUP_NAME \
+    --name $WINDOWS_POOL_NAME \
+    --node-count 1 \
+    --node-vm-size standard_d4ads_v5 \
+    --node-osdisk-size 250 \
+    --os-type Windows \
+    --os-sku Windows2022 \
+    --max-pods 250 \
+    --no-wait
+
+# Wait until Apps nodepool is ready
+
+az aks nodepool update \
+    --cluster-name $CLUSTER_NAME \
+    --resource-group $RESOURCE_GROUP_NAME \
+    --name $APPS_POOL_NAME \
+    --labels nginx="true"
 
 KUBELET_CLIENT_ID=$(az aks show --resource-group $RESOURCE_GROUP_NAME --name $CLUSTER_NAME --query identityProfile.kubeletidentity.clientId -o tsv)
 KUBELET_OBJECT_ID=$(az aks show --resource-group $RESOURCE_GROUP_NAME --name $CLUSTER_NAME --query identityProfile.kubeletidentity.objectId -o tsv)
@@ -76,11 +94,11 @@ kubelogin convert-kubeconfig -l azurecli
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-pwd=$(k get secret -n argocd argocd-initial-admin-secret -o json | jq .data.password -r | base64 -d)
-echo $pwd 
+argocdPwd=$(k get secret -n argocd argocd-initial-admin-secret -o json | jq .data.password -r | base64 -d)
+echo $argocdPwd 
 
 k port-forward svc/argocd-server -n argocd 8080:443
-argocd login localhost:8080 --username admin --password $pwd
+argocd login localhost:8080 --username admin --password $argocdPwd
 
 # Demo app, just to have something simple and minimal. Has nothing to do with Loki.
 argocd app create nginx \
@@ -124,8 +142,18 @@ argocd app create logger \
     --sync-policy automated \
     --sync-option CreateNamespace=true
 
+# Has nothing to do with Loki
+argocd app create aspnet \
+    --repo https://github.com/johankardell/argocd-demo \
+    --path ./aspnet/ \
+    --dest-namespace aspnet \
+    --dest-server https://kubernetes.default.svc \
+    --sync-policy automated \
+    --sync-option CreateNamespace=true
+
 # Grafana (default user admin)
-k get secret -n grafana grafana -o json | jq '.data.["admin-password"]' -r | base64 -d
+grafanaPwd=$(k get secret -n grafana grafana -o json | jq '.data.["admin-password"]' -r | base64 -d)
+echo $grafanaPwd
 k port-forward svc/grafana -n grafana 3000:80
 # Add Loki as source in Grafana: http://loki-gateway.loki.svc
 
