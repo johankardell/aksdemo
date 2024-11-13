d2b825726256f62329c0471e629d524b404f7086
Johan Kardell <johan.kardell@gmail.com>
2024-11-13 13:49:06 +0100

    Update script

--- gitopsEnterprise/deploy.sh
+++ gitopsEnterprise/deploy.sh
@@ -1,9 +1,7 @@
 sshKey=`cat ~/.ssh/id_rsa.pub`
 location="francecentral"
 
-# IP="$(curl ipinfo.io/ip 2>/dev/null)"
-
-az account set -s aks
+IP="$(curl ipinfo.io/ip 2>/dev/null)"
 
 ## Deployment stack
 az stack sub create --deny-settings-mode none --action-on-unmanage deleteAll --location "$location" --template-file main.bicep -n gitopsEnterprise --yes --parameters sshkey="$sshKey" location="$location" 
