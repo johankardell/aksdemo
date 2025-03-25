77fee7bde8b7760c65334a77d70a479bdad6e350
Johan Kardell <johan.kardell@gmail.com>
2025-03-25 14:24:33 +0100

    Update fio params and benchmark result

--- aksStorage/deploy.sh
+++ aksStorage/deploy.sh
@@ -1,6 +1,6 @@
 # https://learn.microsoft.com/en-us/azure/storage/container-storage/use-container-storage-with-local-disk#5-deploy-a-pod-and-attach-a-persistent-volume
 
-az extension add --name fleet --allow-preview true
+# az extension add --name fleet --allow-preview true
 
 export SUBSCRIPTION_ID=$(az account show --name one --query id -o tsv)
 export SUBSCRIPTION_ID1=$(az account show --name one --query id -o tsv )
@@ -32,7 +32,7 @@ az fleet create --resource-group ${RESOURCE_GROUP} --name ${FLEET} --location $L
 (
     az group create --name ${AKS_RESOURCE_GROUP} --location $LOCATION --subscription $SUBSCRIPTION_ID1
     az aks create --resource-group $AKS_RESOURCE_GROUP --name $AKS_CLUSTER_NAME_1 --node-count 2 --kubernetes-version $K8s_version --ssh-access disabled --node-vm-size Standard_D4lds_v5 --node-osdisk-type Ephemeral --nodepool-taints "CriticalAddonsOnly=true:NoSchedule" --subscription $SUBSCRIPTION_ID1
-    az aks nodepool add --resource-group $AKS_RESOURCE_GROUP --cluster-name $AKS_CLUSTER_NAME_1 --ssh-access disabled --name storage --node-count 3 --node-vm-size Standard_D8lds_v6 --os-type Linux --os-sku AzureLinux --subscription $SUBSCRIPTION_ID1
+    az aks nodepool add --resource-group $AKS_RESOURCE_GROUP --cluster-name $AKS_CLUSTER_NAME_1 --ssh-access disabled --name storage --node-count 3 --node-vm-size Standard_D8lds_v6 --node-osdisk-type Ephemeral --os-type Linux --os-sku AzureLinux --subscription $SUBSCRIPTION_ID1
     az fleet member create --resource-group ${RESOURCE_GROUP} --fleet-name ${FLEET} --name ${AKS_CLUSTER_NAME_1} --member-cluster-id ${MEMBER_CLUSTER_ID_1} --subscription $SUBSCRIPTION_ID
     az aks update -n $AKS_CLUSTER_NAME_1 -g $AKS_RESOURCE_GROUP --enable-azure-container-storage ephemeralDisk --storage-pool-option nvme --azure-container-storage-nodepools storage --no-wait --subscription $SUBSCRIPTION_ID1
     az aks get-credentials -n $AKS_CLUSTER_NAME_1 -g $AKS_RESOURCE_GROUP --subscription $SUBSCRIPTION_ID1 --overwrite-existing
@@ -81,7 +81,7 @@ wait
 # benchmark - the manifests deploys a pod named fiopod. Exec into it and run.
 k exec -it fiopod -- /bin/bash
 apt update && apt install -y fio
-fio --name=benchtest --size=800m --filename=/volume/test --direct=1 --rw=randrw --ioengine=libaio --bs=4k --iodepth=16 --numjobs=8 --time_based --runtime=60
+fio --directory=/volume --size=4000MB --filename_format='testfile.$jobnum' --wait_for_previous --thread --group_reporting --direct=1 --randrepeat=0 --norandommap=1 --ioengine=io_uring --numjobs=8 --name=precondition --bs=1M --iodepth=64 --rw=write --name=randwritebench --rw=randwrite --bs=4k --iodepth=16 --time_based --runtime=60 --name=randreadbench --rw=randread --bs=4k --iodepth=16 --time_based --runtime=60 --name=seqwritebench --rw=write --bs=128k --iodepth=16 --time_based --runtime=60 --name=seqreadbench --rw=read --bs=128k --iodepth=16 --time_based --runtime=60
 
 
 
